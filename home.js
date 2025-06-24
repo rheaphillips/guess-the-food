e18cdea6957cd22a45c1ae2edc4683ea1b206ace
Phillips <rheaphillips08@gmail.com>
2025-06-24 11:16:33 -0400

    Create modal box for instructions

---
+++ home.js
@@ -0,0 +1,19 @@
+const modal = document.querySelector('.modal'), overlay = document.querySelector('.overlay');
+
+document.querySelector('.instructions-button').addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+});
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+overlay.addEventListener('click', closeModal);
+
+document.querySelector('.close-modal').addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === "Escape") closeModal();
+})
