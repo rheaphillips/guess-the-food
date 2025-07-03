5774cc4cc69219ee255e1dc25eb4f69a97567a4c
Phillips <rheaphillips08@gmail.com>
2025-07-03 16:44:31 -0400

    Change navbar layout for screens with a small width

--- js/gamePage.js
+++ js/gamePage.js
@@ -1,6 +1,6 @@
 let groceryWords = ["apple", "grape", "peach", "mango", "lemon", "onion", "beans", "chard", "bacon", "steak", "roast", "cream", "gouda", "bread", "bagel", "pasta", "penne", "ramen", "flour", "sugar", "honey", "cumin", "thyme", "basil", "jelly", "yeast", "cocoa", "chips", "candy", "dates", "sushi", "juice", "broth", "stock", "mochi", "salsa", "pesto", "gravy", "decaf", "latte", "oreos", "guava", "pizza", "toast", "pears", "olive", "salad", "berry", "sauce", "spice", "wafer", "hazel", "curry", "cider", "tacos", "water", "melon", "limes", "beets", "snail", "leeks", "mints", "herbs", "grits", "crabs", "donut", "wheat", "fries", "cacao", "fudge", "icing", "scone", "pecan", "cakes", "kebab", "wings", "nacho", "chive", "dairy", "clams", "fruit", "crepe", "seeds", "pitas", "tarts", "prune", "mocha", "syrup", "ranch", "clove", "eggos", "okras", "plums", "prawn", "rolls", "tikka", "penne", "squid", "wraps", "tapas", "jello", "trout", "grain", "meats", "humus", "saute"];
 
-let classes = ['grid', 'confetti-container', 'overlay', 'win-modal', 'lose-modal', 'keyboard', 'play-again', 'play-again-lose', 'hint-button', 'hint-image', 'restart-button'], elems = {}, colourStates = ['correct', 'present', 'absent'];
+let classes = ['grid', 'confetti-container', 'overlay', 'win-modal', 'lose-modal', 'keyboard', 'play-again', 'play-again-lose', 'hint-button', 'hint-image', 'restart-button', 'menu-button', 'close-button'], elems = {}, colourStates = ['correct', 'present', 'absent'];
 
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(''), keyLayout = [['Q','W','E','R','T','Y','U','I','O','P'], ['A','S','D','F','G','H','J','K','L'], ['Enter','Z','X','C','V','B','N','M','backspace']];
 
@@ -47,6 +47,27 @@ const createKeyboard = function () {
 
     elems.keyboard.appendChild(rowEl);
   });
+}
+
+const revealNavbar = function () {
+  let links = document.querySelector('.nav-btns-phone');
+  let logo = document.querySelector('.logo-image');
+  links.style.display = 'flex';
+  links.style.flexDirection = 'row';
+  links.style.justifyContent = 'space-evenly';
+  links.style.alignItems = 'center';
+  logo.style.display = 'none';
+  elems.menuButton.classList.add('hidden');
+  elems.overlay.classList.remove('hidden');
+}
+
+const closeNavbar = function () {
+  let links = document.querySelector('.nav-btns-phone');
+  let logo = document.querySelector('.logo-image');
+  links.style.display = 'none';
+  logo.style.display = 'inline';
+  elems.menuButton.classList.remove('hidden');
+  elems.overlay.classList.add('hidden');
 }
 
 const resetRow = function () {
@@ -148,6 +169,8 @@ const createEventListeners = function () {
     this.blur();
     giveHint();
   });
+  elems.menuButton.addEventListener('click', revealNavbar);
+  elems.closeButton.addEventListener('click', closeNavbar);
 }
 
 const reset = function () {
